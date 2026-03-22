304dc47e5d1b2c24d8750148615c94bef69409da
= <=>
2026-03-23 10:28:17 +1300

    change

--- Lab 5/Program.cs
+++ Lab 5/Program.cs
@@ -1,3 +1,38 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
-Console.WriteLine("hey");
+public class Lab05
+{
+    public static void Main()
+    {
+        IPaymentMethod method = new CreditCardPayment();
+        method.Pay(100);
+    }
+
+    public interface IPaymentMethod
+    {
+        void Pay(decimal amount);
+    }
+    public class CreditCardPayment : IPaymentMethod
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} with Credit Card");
+        }
+    }
+    public class PayPalPayment : IPaymentMethod
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} with Credit Card");
+        }
+    }
+    public class ReceiptSender
+    {
+        public void SendReceipt(string email, decimal amount)
+        {
+            Console.WriteLine($"Receipt for {amount} sent to {email}");
+        }
+    }
+}
--- Lab 5/obj/Debug/net8.0/Lab 5.AssemblyInfo.cs
+++ Lab 5/obj/Debug/net8.0/Lab 5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab 5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a8eac5250713cc93c5b80637eb2d1cbf37ce60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab 5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab 5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
